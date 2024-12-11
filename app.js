b65e09e4f14b5d400e90224baf80c4632f9d4524
Joris Bertier <joris.bertier@gmail.com>
2024-12-11 08:47:02 -0600

    learn middleware

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Requete recu');
+    next();
+})
 
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' }); 
+    next();
+});
 
+app.use((req, res) => {
+    console.log('requete envoye avec succes')
+})
 module.exports = app;
